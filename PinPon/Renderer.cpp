68ad70a17501c48421ebc52e5298a294e028ea0f
wakame3255 <46024199+wakame3255@users.noreply.github.com>
2025-06-25 14:00:28 +0900

    レンダラーの責任分離

--- PinPon/Renderer.cpp
+++ PinPon/Renderer.cpp
@@ -1,1 +1,15 @@
-#include "Renderer.h";
+#include "Renderer.h"
+
+ void Renderer::DrawPaddles(const std::vector<SDL_Rect>& paddles)
+{
+     // ƒpƒhƒ‹‚Ì•`‰æ
+     for (const auto& paddlePos : paddles) {
+         SDL_Rect paddle{
+             static_cast<int>(paddlePos.x - 10),
+             static_cast<int>(paddlePos.y),
+             GameConstants::PADDLE_WIDTH,
+             GameConstants::PADDLE_HEIGHT
+         };
+         SDL_RenderFillRect(mRenderer, &paddle);
+     }
+}
--- PinPon/Renderer.h
+++ PinPon/Renderer.h
@@ -1,10 +1,14 @@
 #pragma once
 #include "Game.h"
 #include "SDL.h"
+#include "UtilityData.h"
 
 class Renderer
 {
-	SDL_Renderer* sdlRenderer;
+	//SDLのウィンドウ
+	SDL_Window* mWindow;
+	//SDLの2Dレンダラー
+	SDL_Renderer* mRenderer;
 
 public:
 	void DrawPaddles(const std::vector<SDL_Rect>& paddles);
